4891446495fca47976fca6b7c16000034678f94b
martinhric78 <tampasarasotahandyman@gmail.com>
2026-08-01 13:44:53 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hhn-prompter-v5';
+const CACHE = 'hhn-prompter-v6';
 const FILES = [
   './', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'
